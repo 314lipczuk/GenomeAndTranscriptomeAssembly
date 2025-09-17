2f69810cf3f4acc96bcd59c0ab9bb07848a7beee
Przemyslaw Pilipczuk <3.14lipczuk@gmail.com>
2025-09-17 14:22:51 +0200

    wip: qc and repo organization

---
+++ code/00_setup.sh
@@ -0,0 +1,7 @@
+
+export BASEDIR="/data/users/ppilipczuk/GenomeAndTransAss"
+export CODEDIR="$BASEDIR/CODE"
+export DATADIR="$BASEDIR/data"
+export LOGDIR="$BASEDIR/logs"
+export RESULTDIR="$BASEDIR/results"
+echo "sourced config.sh"
---
+++ code/01_qc.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH --time=15:00:00
+#SBATCH --mem=4g
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=QualityControl
+#SBATCH --partition=pibu_el8
+#SBATCH --mail-user=przemyslaw.pilipczuk@students.unibe.ch
+#SBATCH --mail-type=end,fail
+
+# The log paths are handled in the run.sh and 00_setup.sh, and set automatically
+
+#module load FastQC/0.11.9-Java-11
+#module load MultiQC/1.11-foss-2021a
+qc_og="$RESULTDIR/qc_original"
+mkdir $qc_og
+
+apptainer exec \
+  --bind /data \
+  /containers/apptainer/fastqc-0.12.1.sif \
+  fastqc "$BASEDIR/reads.fastq.gz" -o $qc_og
+
+apptainer exec \
+  --bind /data \
+  /containers/apptainer/fastqc-0.12.1.sif \
+  fastqc "$BASEDIR/RNA_data/ERR754081_2.fastq.gz" -o $qc_og
+
+apptainer exec \
+  --bind /data \
+  /containers/apptainer/fastqc-0.12.1.sif \
+  fastqc "$BASEDIR/RNA_data/ERR754081_1.fastq.gz" -o "$qc_og"
+  
+
+fp="$RESULTDIR/fastp"
+mkdir $fp
+mkdir "$fp/dna"
+mkdir "$fp/rna1"
+mkdir "$fp/rna2"
+
+apptainer exec \
+  --bind /data \
+  /containers/apptainer/fastp_0.23.2--h5f740d0_3.sif \
+  fastp -Q -i "$BASEDIR/reads.fastq.gz" -o "$fp/dna/report.html"
+
+apptainer exec \
+  --bind /data \
+  /containers/apptainer/fastp_0.23.2--h5f740d0_3.sif \
+  fastp -i "$BASEDIR/rna_data/ERR754081_1.fastq.gz" -o "$fp/rna1/report.html"  
+
+apptainer exec \
+  --bind /data \
+  /containers/apptainer/fastp_0.23.2--h5f740d0_3.sif \
+  fastp -i "$BASEDIR/rna_data/ERR754081_2.fastq.gz" -o "$fp/rna2/report.html"
